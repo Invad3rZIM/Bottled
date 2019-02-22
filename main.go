2f4e94eef3e60073e607e201c9fb5185b429dc0d
kirk <kzimmer97@github.com>
2019-02-21 19:47:53 -0500

    dependencies

--- main.go
+++ main.go
@@ -4,17 +4,21 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/go-chi/chi"
 )
 
 func main() {
-	http.HandleFunc("/", hello)
+	r := chi.NewRouter()
+	r.Get("/", hello)
+
 	fmt.Println("listening...")
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), r)
 	if err != nil {
 		panic(err)
 	}
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "hello, world")
+	fmt.Fprintln(res, "hello rob")
 }
